3eff586724c0476795d28129424a1255d6d5ae2c
Mike <Mike@Aorus_F39>
2024-05-14 18:31:21 +0200

    Trying to find alternative to queue

---
+++ tests/solve_set_outliers.cpp
@@ -0,0 +1,56 @@
+/*
+ * solve_set_outliers.cpp
+ * 
+ * Copyright 2024 mike <mike@f39evo970-1.home>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+// Local definition
+
+#include <iostream>
+#include <cstdint>
+#include <set>		// 
+
+typedef uint64_t T;
+
+#include "classBaseN.hpp"
+
+//--------------------Main----------------------------------------------
+
+int main(int argc, char **argv)
+{
+	// Must specify typename T, else it defaults to int 
+	// which causes overflow.
+	BaseNCounter<T> bnc(3);	//base3 counter
+	bnc.set_accum(2);
+    
+    set<T> s;
+    set<T>::iterator i1, i2;
+    for(T a = 2; a <= 100; ++a) s.insert(a);
+    
+    i1 = s.begin();
+    while(s.empty == false) {
+        T divisor = *i1;
+        if(bnc.get_accum % divisor) == 0 {
+            
+    
+	return 0;
+}
+
