59ff8a14441d501c84ba193ae6c51280925cf53d
mike <mike@f39evo970-1.home>
2024-05-18 16:05:28 +0200

    Rewrite of the database approach

--- tests/b3nfactors.cpp
+++ tests/b3nfactors.cpp
@@ -12,6 +12,8 @@
 #include <cstdint>
 #include <vector>
 #include <set>
+#include <iterator>		// std::next
+#include <algorithm>	// std::reverse
 
 #include "./classPrime.hpp"
 #include "./classBaseN.hpp"
@@ -22,10 +24,12 @@ void find_combinations(vector<uint64_t> source,
 					   vector<uint64_t>& combinations, bool verbose);
 					   
 // Definitions
-void prt_factors(vector<T> pf){
+void prt_vector(vector<T> pf){
+	if(!(pf.empty())){
 	for(T p : pf) cout << "  " << p;
 	cout << endl;
 	}
+}
 
 void find_combinations(vector<uint64_t> source, 
 					   vector<uint64_t>& combinations, bool verbose){
@@ -55,7 +59,10 @@ void find_combinations(vector<uint64_t> source,
     }
     // remove duplicates and order combinations;
     set<uint64_t> s(combinations.begin(), combinations.end());
-    combinations.assign(s.begin(), --s.end());
+    // Program Specific - reverse order
+    combinations.assign(s.rbegin(), --s.rend());
+    // Normal Order
+    //combinations.assign(s.begin(), --s.end());
 }
 
 typedef uint64_t T;
@@ -64,21 +71,35 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	Primes<T> primes(100001);
+	const T Limit = 1000;
+	Primes<T> primes(Limit*10 + 1);
+	set<T> dbase;
+	
+	for(T i = 11; i != Limit+1; ++i) dbase.insert(dbase.end(), i);
+	
 	BaseNCounter bcn(3);
-	bcn.set_accum(100);
+	bcn.set_accum(10);
 	T b3n = bcn.get_accum();
-	while(b3n < 100000){
+	while(b3n <= 22'222'222'222){
 		bcn.inc_accum();
 		b3n = bcn.get_accum();
 		vector<T> pf = primes.prime_factors(b3n);
-		prt_factors(pf);
+		//prt_vector(pf);
 		vector<T> combinations;
 		find_combinations(pf, combinations, false);
-		cout << b3n << " | ";
-		prt_factors(combinations);
+		// Eliminate combinations from the database set
+		for(T c : combinations){
+			if(c > Limit) continue;
+			if(c < 11) break;
+			auto i = dbase.find(c);
+			if(i != dbase.end()){
+				cout << "Erasing " << c << endl;
+				dbase.erase(i);
+				//if(dbase.empty()) break;
+			}
+		}
 	}
-	
+	for(auto n : dbase) cout << " " << n;
+	cout << endl;
 	return 0;
 }
-
